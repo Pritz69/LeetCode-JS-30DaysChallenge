32905dd4e2bc641709637e0c82b2ddae89faefa2
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-05-05 12:48:39 +0530

    Update Create_Hello_World_Function.js

--- Create_Hello_World_Function.js
+++ Create_Hello_World_Function.js
@@ -1,1 +1,19 @@
+# Function Syntax
 
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    }
+};
+
+# Arrow Syntax
+
+var createHelloWorld = function() {
+    return () => "Hello World";
+};
+
+# Arrow Syntax + Rest Arguments
+
+var createHelloWorld = function() {
+    return (...args) => "Hello World";
+};
